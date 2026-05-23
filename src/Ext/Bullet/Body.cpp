cncrazer/phobos

commit 838c2e7b7997e9e771e0f93ac7070c93e83ba472

[Vanilla Enhancement] Enhance the laser, ebolt and rbeam drawing (#1553)
- Fix the bug that laser, electric bolt and rad beam not support
`FlakScatter=true` and `Inaccurate=true` projectiles.

---

- You can now add a random offset to visual effect's (`IsLaser=true`,
`IsElectricBolt=true` or `IsRadBeam=true`) target location if set
`VisualScatter` to true.

In `rulesmd.ini`:
```ini
[AudioVisual]
VisualScatter.Min=0.03  ; floating point value, distance in cells
VisualScatter.Max=0.13  ; floating point value, distance in cells

[SOMEWEAPON]            ; WeaponType
VisualScatter=false     ; boolean
```

src/Ext/Bullet/Body.cpp
@@ -239,14 +239,14 @@ inline void BulletExt::SimulatedFiringLaser(BulletClass* pBullet, HouseClass* pH
 	if (pWeapon->IsHouseColor || pWeaponExt->Laser_IsSingleColor)
 	{
 		const auto black = ColorStruct { 0, 0, 0 };
-		const auto pLaser = GameCreate<LaserDrawClass>(pBullet->SourceCoords, pBullet->TargetCoords, ((pWeapon->IsHouseColor && pHouse) ? pHouse->LaserColor : pWeapon->LaserInnerColor), black, black, pWeapon->LaserDuration);
+		const auto pLaser = GameCreate<LaserDrawClass>(pBullet->SourceCoords, pBullet->Data.Location, ((pWeapon->IsHouseColor && pHouse) ? pHouse->LaserColor : pWeapon->LaserInnerColor), black, black, pWeapon->LaserDuration);
 		pLaser->IsHouseColor = true;
 		pLaser->Thickness = pWeaponExt->LaserThickness;
 		pLaser->IsSupported = (pLaser->Thickness > 3);
 	}
 	else
 	{
-		const auto pLaser = GameCreate<LaserDrawClass>(pBullet->SourceCoords, pBullet->TargetCoords, pWeapon->LaserInnerColor, pWeapon->LaserOuterColor, pWeapon->LaserOuterSpread, pWeapon->LaserDuration);
+		const auto pLaser = GameCreate<LaserDrawClass>(pBullet->SourceCoords, pBullet->Data.Location, pWeapon->LaserInnerColor, pWeapon->LaserOuterColor, pWeapon->LaserOuterSpread, pWeapon->LaserDuration);
 		pLaser->IsHouseColor = false;
 		pLaser->Thickness = 3;
 		pLaser->IsSupported = false;
@@ -268,7 +268,7 @@ inline void BulletExt::SimulatedFiringElectricBolt(BulletClass* pBullet)
 	auto& weaponStruct = WeaponTypeExt::BoltWeaponMap[pEBolt];
 	weaponStruct.Weapon = WeaponTypeExt::ExtMap.Find(pWeapon);
 	weaponStruct.BurstIndex = 0;
-	pEBolt->Fire(pBullet->SourceCoords, pBullet->TargetCoords, 0);
+	pEBolt->Fire(pBullet->SourceCoords, pBullet->Data.Location, 0);
 }
 
 // Make sure pBullet and pBullet->WeaponType is not empty before call
@@ -284,7 +284,7 @@ inline void BulletExt::SimulatedFiringRadBeam(BulletClass* pBullet, HouseClass* 
 	const auto pRadBeam = RadBeam::Allocate(isTemporal ? RadBeamType::Temporal : RadBeamType::RadBeam);
 
 	pRadBeam->SetCoordsSource(pBullet->SourceCoords);
-	pRadBeam->SetCoordsTarget(pBullet->TargetCoords);
+	pRadBeam->SetCoordsTarget(pBullet->Data.Location);
 
 	const auto pWeaponExt = WeaponTypeExt::ExtMap.Find(pWeapon);
 
@@ -297,7 +297,7 @@ inline void BulletExt::SimulatedFiringRadBeam(BulletClass* pBullet, HouseClass* 
 inline void BulletExt::SimulatedFiringParticleSystem(BulletClass* pBullet, HouseClass* pHouse)
 {
 	if (const auto pPSType = pBullet->WeaponType->AttachedParticleSystem)
-		GameCreate<ParticleSystemClass>(pPSType, pBullet->SourceCoords, pBullet->Target, pBullet->Owner, pBullet->TargetCoords, pHouse);
+		GameCreate<ParticleSystemClass>(pPSType, pBullet->SourceCoords, pBullet->Target, pBullet->Owner, pBullet->Data.Location, pHouse);
 }
 
 // Make sure pBullet is not empty before call

src/Ext/Techno/Hooks.WeaponEffects.cpp
@@ -15,10 +15,18 @@
 
 namespace FireAtTemp
 {
+	BulletClass* FireBullet = nullptr;
 	CoordStruct originalTargetCoords;
 	CellClass* pObstacleCell = nullptr;
 	AbstractClass* pOriginalTarget = nullptr;
 	AbstractClass* pWaveOwnerTarget = nullptr;
+}
+
+DEFINE_HOOK(0x6FF08B, TechnoClass_Fire_RecordBullet, 0x6)
+{
+	GET(BulletClass*, pBullet, EBX);
+	FireAtTemp::FireBullet = pBullet;
+	return 0;
 }
 
 // Set obstacle cell.
@@ -190,18 +198,34 @@ DEFINE_HOOK(0x62B8BC, ParticleClass_CTOR_CoordAdjust, 0x6)
 	return 0;
 }
 
-// Adjust target coordinates for laser drawing.
-DEFINE_HOOK(0x6FD38D, TechnoClass_LaserZap_Obstacles, 0x7)
+DEFINE_HOOK_AGAIN(0x6FD70D, TechnoClass_DrawSth_DrawToInvisoFlakScatterLocation, 0x6) // CreateRBeam
+DEFINE_HOOK_AGAIN(0x6FD514, TechnoClass_DrawSth_DrawToInvisoFlakScatterLocation, 0x7) // CreateEBolt
+DEFINE_HOOK(0x6FD38D, TechnoClass_DrawSth_DrawToInvisoFlakScatterLocation, 0x7) // CreateLaser
 {
 	GET(CoordStruct*, pTargetCoords, EAX);
 
-	auto coords = *pTargetCoords;
-	auto const pObstacleCell = FireAtTemp::pObstacleCell;
+	if (const auto pBullet = FireAtTemp::FireBullet)
+	{
+		// The weapon may not have been set up
+		const auto pWeaponExt = WeaponTypeExt::ExtMap.Find(pBullet->WeaponType);
+		// pBullet->Data.Location (0x4E1130) -> pBullet->Type->Inviso ? pBullet->Location : pBullet->TargetCoords
+		if (pWeaponExt && pWeaponExt->VisualScatter)
+		{
+			const auto& pRulesExt = RulesExt::Global();
+			const auto radius = ScenarioClass::Instance->Random.RandomRanged(pRulesExt->VisualScatter_Min.Get(), pRulesExt->VisualScatter_Max.Get());
+			*pTargetCoords = MapClass::GetRandomCoordsNear(pBullet->Data.Location, radius, false);
+		}
+		else
+		{
+			*pTargetCoords = pBullet->Data.Location;
+		}
+	}
+	else if (const auto pObstacleCell = FireAtTemp::pObstacleCell)
+	{
+		*pTargetCoords = pObstacleCell->GetCoordsWithBridge();
+	}
 
-	if (pObstacleCell)
-		coords = pObstacleCell->GetCoordsWithBridge();
-
-	R->EAX(&coords);
+	R->EAX(pTargetCoords);
 	return 0;
 }
 
@@ -235,6 +259,7 @@ DEFINE_HOOK(0x6FF660, TechnoClass_FireAt_ObstacleCellUnset, 0x6)
 	R->EDI(FireAtTemp::pOriginalTarget);
 
 	// Reset temp values
+	FireAtTemp::FireBullet = nullptr;
 	FireAtTemp::originalTargetCoords = CoordStruct::Empty;
 	FireAtTemp::pObstacleCell = nullptr;
 	FireAtTemp::pOriginalTarget = nullptr;

src/Ext/Rules/Body.cpp
@@ -192,6 +192,9 @@ void RulesExt::ExtData::LoadBeforeTypeData(RulesClass* pThis, CCINIClass* pINI)
 
 	this->BuildingWaypoints.Read(exINI, GameStrings::General, "BuildingWaypoints");
 
+	this->VisualScatter_Min.Read(exINI, GameStrings::AudioVisual, "VisualScatter.Min");
+	this->VisualScatter_Max.Read(exINI, GameStrings::AudioVisual, "VisualScatter.Max");
+
 	this->Buildings_DefaultDigitalDisplayTypes.Read(exINI, GameStrings::AudioVisual, "Buildings.DefaultDigitalDisplayTypes");
 	this->Infantry_DefaultDigitalDisplayTypes.Read(exINI, GameStrings::AudioVisual, "Infantry.DefaultDigitalDisplayTypes");
 	this->Vehicles_DefaultDigitalDisplayTypes.Read(exINI, GameStrings::AudioVisual, "Vehicles.DefaultDigitalDisplayTypes");
@@ -413,6 +416,8 @@ void RulesExt::ExtData::Serialize(T& Stm)
 		.Process(this->Infantry_DefaultDigitalDisplayTypes)
 		.Process(this->Vehicles_DefaultDigitalDisplayTypes)
 		.Process(this->Aircraft_DefaultDigitalDisplayTypes)
+		.Process(this->VisualScatter_Min)
+		.Process(this->VisualScatter_Max)
 		.Process(this->ShowDesignatorRange)
 		.Process(this->DropPodTrailer)
 		.Process(this->PodImage)

src/Ext/WeaponType/Body.cpp
@@ -97,6 +97,7 @@ void WeaponTypeExt::ExtData::LoadFromINIFile(CCINIClass* const pINI)
 	this->AreaFire_Target.Read(exINI, pSection, "AreaFire.Target");
 	this->FeedbackWeapon.Read<true>(exINI, pSection, "FeedbackWeapon");
 	this->Laser_IsSingleColor.Read(exINI, pSection, "IsSingleColor");
+	this->VisualScatter.Read(exINI, pSection, "VisualScatter");
 	this->ROF_RandomDelay.Read(exINI, pSection, "ROF.RandomDelay");
 	this->ChargeTurret_Delays.Read(exINI, pSection, "ChargeTurret.Delays");
 	this->OmniFire_TurnToTarget.Read(exINI, pSection, "OmniFire.TurnToTarget");
@@ -154,6 +155,7 @@ void WeaponTypeExt::ExtData::Serialize(T& Stm)
 		.Process(this->AreaFire_Target)
 		.Process(this->FeedbackWeapon)
 		.Process(this->Laser_IsSingleColor)
+		.Process(this->VisualScatter)
 		.Process(this->ROF_RandomDelay)
 		.Process(this->ChargeTurret_Delays)
 		.Process(this->OmniFire_TurnToTarget)

src/Ext/Rules/Body.h
@@ -142,6 +142,9 @@ public:
 		ValueableVector<DigitalDisplayTypeClass*> Vehicles_DefaultDigitalDisplayTypes;
 		ValueableVector<DigitalDisplayTypeClass*> Aircraft_DefaultDigitalDisplayTypes;
 
+		Valueable<Leptons> VisualScatter_Min;
+		Valueable<Leptons> VisualScatter_Max;
+
 		Valueable<bool> ShowDesignatorRange;
 		Valueable<bool> IsVoiceCreatedGlobal;
 		Valueable<int> SelectionFlashDuration;
@@ -309,6 +312,8 @@ public:
 			, Infantry_DefaultDigitalDisplayTypes {}
 			, Vehicles_DefaultDigitalDisplayTypes {}
 			, Aircraft_DefaultDigitalDisplayTypes {}
+			, VisualScatter_Min { Leptons(8) }
+			, VisualScatter_Max { Leptons(32) }
 			, ShowDesignatorRange { true }
 			, DropPodTrailer { }
 			, PodImage { }

src/Ext/WeaponType/Body.h
@@ -43,6 +43,7 @@ public:
 		Valueable<AreaFireTarget> AreaFire_Target;
 		Valueable<WeaponTypeClass*> FeedbackWeapon;
 		Valueable<bool> Laser_IsSingleColor;
+		Valueable<bool> VisualScatter;
 		Nullable<PartialVector2D<int>> ROF_RandomDelay;
 		ValueableVector<int> ChargeTurret_Delays;
 		Valueable<bool> OmniFire_TurnToTarget;
@@ -96,6 +97,7 @@ public:
 			, AreaFire_Target { AreaFireTarget::Base }
 			, FeedbackWeapon {}
 			, Laser_IsSingleColor { false }
+			, VisualScatter { false }
 			, ROF_RandomDelay {}
 			, ChargeTurret_Delays {}
 			, OmniFire_TurnToTarget { false }

commit 9ce797b6d5768d30f707106e93f01dff52c9edfc

Fix the target point position error in visual effect drawing introduced by #1553. (#2222)
Fix the bug caused by #1553 where the visual effect rendering ignores
`[BuildingType] -> TargetCoordOffset=`. Also strictly constrain the
condition to the scenario of `Inviso` && `FlakScatter` rather than
`Inviso` alone.
This change was added between DevBuild#46 and DevBuild#47, later than
Release v0.4, so no documentation changes are required.

In the previous commit (96c6550b9f8534a209910da14e76509bbeab36df) before
merging #1553:
<img width="270" height="377" alt="image"
src="https://github.com/user-attachments/assets/2138628a-87f5-4b8e-b18f-c1860d7ce8d1"
/>

Latest development branch nightly:
<img width="271" height="361" alt="image"
src="https://github.com/user-attachments/assets/2762fbc1-09c5-47f0-ada1-4d13839731a6"
/>

This PR:
<img width="280" height="393" alt="image"
src="https://github.com/user-attachments/assets/cedf0bca-e75e-477c-b9ca-f2d71c1bc4ca"
/>

src/Ext/Bullet/Body.cpp
@@ -207,7 +207,7 @@ inline void BulletExt::SimulatedFiringLaser(BulletClass* pBullet, HouseClass* pH
 	if (pWeapon->IsHouseColor || pWeaponExt->Laser_IsSingleColor)
 	{
 		const auto black = ColorStruct { 0, 0, 0 };
-		const auto pLaser = GameCreate<LaserDrawClass>(pBullet->SourceCoords, (pBullet->Type->Inviso ? pBullet->Location : pBullet->TargetCoords),
+		const auto pLaser = GameCreate<LaserDrawClass>(pBullet->SourceCoords, ((pBullet->Type->Inviso && pBullet->Type->FlakScatter) ? pBullet->Location : abstract_cast<ObjectClass*>(pBullet->Target)->GetTargetCoords()),
 			((pWeapon->IsHouseColor && pHouse) ? pHouse->LaserColor : pWeapon->LaserInnerColor), black, black, pWeapon->LaserDuration);
 
 		pLaser->IsHouseColor = true;
@@ -216,7 +216,7 @@ inline void BulletExt::SimulatedFiringLaser(BulletClass* pBullet, HouseClass* pH
 	}
 	else
 	{
-		const auto pLaser = GameCreate<LaserDrawClass>(pBullet->SourceCoords, (pBullet->Type->Inviso ? pBullet->Location : pBullet->TargetCoords),
+		const auto pLaser = GameCreate<LaserDrawClass>(pBullet->SourceCoords, ((pBullet->Type->Inviso && pBullet->Type->FlakScatter) ? pBullet->Location : abstract_cast<ObjectClass*>(pBullet->Target)->GetTargetCoords()),
 			pWeapon->LaserInnerColor, pWeapon->LaserOuterColor, pWeapon->LaserOuterSpread, pWeapon->LaserDuration);
 
 		pLaser->IsHouseColor = false;
@@ -237,7 +237,7 @@ inline void BulletExt::SimulatedFiringElectricBolt(BulletClass* pBullet)
 	const auto pBolt = EBoltExt::CreateEBolt(pWeapon);
 	pBolt->AlternateColor = pWeapon->IsAlternateColor;
 
-	const auto targetCoords = pBullet->Type->Inviso ? pBullet->Location : pBullet->TargetCoords;
+	const auto targetCoords = (pBullet->Type->Inviso && pBullet->Type->FlakScatter) ? pBullet->Location : abstract_cast<ObjectClass*>(pBullet->Target)->GetTargetCoords();
 	const auto pWeaponExt = WeaponTypeExt::ExtMap.Find(pWeapon);
 	int zAdjust = pWeaponExt->EBoltZAdjust.Get(RulesExt::Global()->EBoltZAdjust);
 
@@ -268,7 +268,7 @@ inline void BulletExt::SimulatedFiringRadBeam(BulletClass* pBullet, HouseClass* 
 	const auto pRadBeam = RadBeam::Allocate(isTemporal ? RadBeamType::Temporal : RadBeamType::RadBeam);
 
 	pRadBeam->SetCoordsSource(pBullet->SourceCoords);
-	pRadBeam->SetCoordsTarget((pBullet->Type->Inviso ? pBullet->Location : pBullet->TargetCoords));
+	pRadBeam->SetCoordsTarget((pBullet->Type->Inviso && pBullet->Type->FlakScatter) ? pBullet->Location : abstract_cast<ObjectClass*>(pBullet->Target)->GetTargetCoords());
 
 	const auto pWeaponExt = WeaponTypeExt::ExtMap.Find(pWeapon);
 
@@ -285,7 +285,7 @@ inline void BulletExt::SimulatedFiringParticleSystem(BulletClass* pBullet, House
 	if (const auto pPSType = pBullet->WeaponType->AttachedParticleSystem)
 	{
 		GameCreate<ParticleSystemClass>(pPSType, pBullet->SourceCoords, pBullet->Target, pBullet->Owner,
-			(pBullet->Type->Inviso ? pBullet->Location : pBullet->TargetCoords), pHouse);
+			((pBullet->Type->Inviso && pBullet->Type->FlakScatter) ? pBullet->Location : abstract_cast<ObjectClass*>(pBullet->Target)->GetTargetCoords()), pHouse);
 	}
 }
 

src/Ext/Techno/Hooks.WeaponEffects.cpp
@@ -231,11 +231,11 @@ DEFINE_HOOK(0x6FD38D, TechnoClass_DrawSth_DrawToInvisoFlakScatterLocation, 0x7) 
 		{
 			const auto& pRulesExt = RulesExt::Global();
 			const auto radius = ScenarioClass::Instance->Random.RandomRanged(pRulesExt->VisualScatter_Min.Get(), pRulesExt->VisualScatter_Max.Get());
-			*pTargetCoords = MapClass::GetRandomCoordsNear((pBullet->Type->Inviso ? pBullet->Location : pBullet->TargetCoords), radius, false);
+			*pTargetCoords = MapClass::GetRandomCoordsNear(((pBullet->Type->Inviso && pBullet->Type->FlakScatter) ? pBullet->Location : abstract_cast<ObjectClass*>(pBullet->Target)->GetTargetCoords()), radius, false);
 		}
 		else
 		{
-			*pTargetCoords = (pBullet->Type->Inviso ? pBullet->Location : pBullet->TargetCoords);
+			*pTargetCoords = ((pBullet->Type->Inviso && pBullet->Type->FlakScatter) ? pBullet->Location : abstract_cast<ObjectClass*>(pBullet->Target)->GetTargetCoords());
 		}
 	}
 	else if (const auto pObstacleCell = FireAtTemp::pObstacleCell)